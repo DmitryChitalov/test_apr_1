066b777476b86cdf8558b15c141c3d976eb7a797
Dmitry Chitalov <35995629+DmitryChitalov@users.noreply.github.com>
2025-04-19 11:01:59 +0500

    Update settings.py

--- market_prj/settings.py
+++ market_prj/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import json
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -48,6 +49,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -87,14 +89,14 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     },
 
-    # 'default': {
-    #     'NAME': 'market_prj_db',
-    #     'ENGINE': 'django.db.backends.postgresql',
-    #     'USER': 'prj_user',
-    #     'PASSWORD': 'prj_user',
-    #     'HOST': 'localhost',
-    #     'PORT': '5434',
-    # }
+    #'default': {
+        #'NAME': 'market_prj_db',
+        #'ENGINE': 'django.db.backends.postgresql',
+        #'USER': 'prj_user',
+        #'PASSWORD': 'prj_user',
+        #'HOST': 'localhost',
+        #'PORT': '5434',
+    #}
 
 }
 
@@ -161,11 +163,16 @@ SOCIAL_AUTH_PIPELINE = (
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
+# Extra lookup directories for collectstatic to find static files
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+prod_db = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
